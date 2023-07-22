d149a215f0deede6260002af1d1f34da5beda613
NabilKuppu <mohammadnabil1510597088@gmail.com>
2023-07-23 02:20:50 +0600

    add then instead of end

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed : ${data}`);
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
+    .then((res) => {
       if (err) return console.log(`API PROBLEM 😥`);
       console.log(res.body.message);
       fs.writeFile(`${__dirname}/dog-img.txt`, res.body.message, (err) => {
         console.log(`File has been written`);
       });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 });

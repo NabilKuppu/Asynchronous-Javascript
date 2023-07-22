de625530087bb8556b7d5af0ab7d1f611ee793cd
NabilKuppu <mohammadnabil1510597088@gmail.com>
2023-07-23 02:31:06 +0600

    read and write promise created

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 const fs = require("fs");
+const { resolve } = require("path");
 const superagent = require("superagent");
 
-//Call Back Hell
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  if (err) return console.log(`I couldn't find that file`);
-  console.log(`Breed : ${data}`);
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      if (err) return console.log(`API PROBLEM 😥`);
-      console.log(res.body.message);
-      fs.writeFile(`${__dirname}/dog-img.txt`, res.body.message, (err) => {
-        console.log(`File has been written`);
-      });
-    })
-    .catch((err) => {
-      console.log(err);
+//Create Promise
+
+//Read File
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(`I couldn't find that file`);
+      resolve(data);
     });
-});
+  });
+};
+// Write File
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      console.log(`File has been written`);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed : ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro(`${__dirname}/dog-img.txt`, res.body.message);
+  })
+  .then(() => {
+    console.log(`File has been written`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+//Call Back Hell
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   if (err) return console.log(`I couldn't find that file`);
+
+// });

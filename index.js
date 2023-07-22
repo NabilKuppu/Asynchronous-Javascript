4c1c5beda608cb6e5e43ecbeaa2c6d0bfa431710
NabilKuppu <mohammadnabil1510597088@gmail.com>
2023-07-23 03:00:47 +0600

    Add mutiple api and consume it

--- index.js
+++ index.js
@@ -27,11 +27,19 @@ const getDogPic = async () => {
   try {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed : ${data}`);
-    const res = await superagent.get(
+    const res1Pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
-    await writeFilePro(`${__dirname}/dog-img.txt`, res.body.message);
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+    await writeFilePro(`${__dirname}/dog-img.txt`, imgs.join("\n"));
     console.log(`File has been written`);
   } catch (err) {
     console.log(err);
